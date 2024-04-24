7add54a84116d55506c67f89f09e14cdbbf1ee76
PeinNotFound <akatsuki_ahmed@yahoo.com>
2024-04-24 21:18:55 +0100

    web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,16 @@
+#setup nginx
+package {
+    'nginx':
+    ensure => installed,
+}
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
+}
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://github.com/alamy2711;\n\t}\n",
+}
+service {'nginx':
+    ensure => running,
+}
